e8b6f245f2ae729f6330416d7b59902e728ae4b0
akash jain <jainakash43@gmail.com>
2020-07-14 01:13:03 +0530

    Files added succesfully

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Headers from './components/Headers.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Headers/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Headers.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import "./Header.css";
+export default function Headers()
+{   
+   const [isNavVisible, setIsNavVisible]=useState(true);
+
+   const [isSmallScreen, setIsSmallScreen]=useState(false);
+
+   useEffect( () =>{
+       
+       const mediaQuery=window.matchMedia("(max-width:700px)");
+       mediaQuery.addListener(handleMediaQueryChange);
+       handleMediaQueryChange(mediaQuery);
+
+       return ()=>{
+           mediaQuery.removeListener(handleMediaQueryChange); 
+       };
+
+
+   },[]);
+
+   const handleMediaQueryChange=(mediaQuery)=>{
+            if(mediaQuery.matches)
+            {
+               setIsSmallScreen(true);
+               setIsNavVisible(false);
+            }
+            else
+            {
+               setIsSmallScreen(false);
+            }
+   };
+
+   const toggleNav=()=>{
+      setIsNavVisible(!isNavVisible);
+   }
+       return (
+          <header className="Header">
+           { (!isSmallScreen || isNavVisible) &&
+               (  
+               <nav className="Nav">
+                <a href="/">Home</a>
+                <a href="/">Articles</a>
+                <a href="/">About</a>
+                <button>Logout</button> 
+               </nav>
+               )
+            }
+            
+            <a  href="#" class="icon" onClick={toggleNav}>
+               <i class="fa fa-bars"></i>
+            </a>
+          </header>
+     );
+
+}
